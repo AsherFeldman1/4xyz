bd8c267a1d8d8b547ae2de22b0456335f4464e5b
AsherFeldman1 <asherfeldman154@gmail.com>
2022-06-21 14:33:43 -0500

    made rate oracle suitable for testing

--- contracts/Vaults/RateOracle.sol
+++ contracts/Vaults/RateOracle.sol
@@ -1,11 +1,23 @@
 pragma solidity ^0.8.4;
 
 contract RateOracle {
+
+	uint public price;
+	int public twap;
+
 	function getPrice(uint _id) public view returns(uint) {
-		return 0;
+		return price;
 	}
 
 	function getTWAP(uint _id) public view returns(int) {
-		return 0;
+		return twap;
+	}
+
+	function setPrice(uint _price) public {
+		price = _price;
+	}
+
+	function setTWAP(int _twap) public {
+		twap = _twap;
 	}
 }
